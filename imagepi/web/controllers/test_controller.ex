9f219cb1a3138f742f99548759fea4a258f0dbb8
Todor Ginchev <todor.a.ginchev@aalto.fi>
2017-04-11 23:28:25 +0300

    Server receiving photo, processind request and returning json reply with the results

--- imagepi/web/controllers/test_controller.ex
+++ imagepi/web/controllers/test_controller.ex
@@ -14,15 +14,19 @@ def create(conn,test_params) do
     Logger.info  "User has sent a photo!"
     #extension = Path.extname(upload.filename)
     imagename = Path.basename(upload.filename)
-    File.cp(upload.path, "/precious/RCH-SPEC-ASSIGN/RCH-SPEC-ASSIGN/imageapi/images/#{imagename}")
-    Logger.info "Image saved to /precious/RCH-SPEC-ASSIGN/RCH-SPEC-ASSIGN/imageapi/images/#{imagename}"
+    File.cp(upload.path,"/precious/RCH-SPEC-ASSIGN/imagepi/images/#{imagename}")
+    Logger.info "Image saved to /precious/RCH-SPEC-ASSIGN/imagepi/images/#{imagename}"
     Logger.info "#{imagename}"
+    response = HTTPotion.post "http://localhost:8000/food_detection/detect/" , [body: "url=/precious/RCH-SPEC-ASSIGN/imagepi/images/#{imagename}", headers: ["User-Agent": "My App", "Content-Type": "application/x-www-form-urlencoded"]]
+    Logger.info "#{HTTPotion.Response.success?(response)}"
+    #Logger.info "#{response.body}"
+    json(conn,response.body)
+  else
+    json(conn,test_params)
   end
+ 
 # response = HTTPotion.get "httpbin.org/get"
-  response = HTTPotion.post "http://localhost:8000/food_detection/detect/" , [body: "url=/home/shishir/foodrec/fooddetect_api/food_detector/data/demo/Egg_Sandwich.jpg", headers: ["User-Agent": "My App", "Content-Type": "application/x-www-form-urlencoded"]]
-  Logger.info "#{HTTPotion.Response.success?(response)}"
-  Logger.info "#{response.body}"
-  json(conn,test_params)
+
 end
 
 end
